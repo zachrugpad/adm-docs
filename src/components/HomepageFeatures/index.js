2bf59a23fbd447b4c893928ff581eb2a21768f40
zachrugpad <zach@rugpadusa.com>
2024-04-30 09:35:57 -0400

    updated featured images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,35 +5,32 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Seamless Integration',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/api-interface.svg').default,
     description: (
       <>
         Our API is crafted to ensure a smooth integration with your existing systems, 
         enabling you to send and receive order and fulfillment data with minimal setup. 
-        Leverage simple, clear API endpoints to enhance your operational efficiency and accuracy.
       </>
     ),
   },
   {
     title: 'Real-Time Updates',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/mobile-app.svg').default,
     description: (
       <>
-        Stay updated with real-time notifications about order shipments and status changes.
-        Our API provides detailed fulfillment updates, ensuring that you always have the most 
-        current information at your fingertips. This helps in managing customer expectations and 
+        Stay updated with real-time notifications about orders.
+        Our API provides customer with detailed fulfillment updates, ensuring customer expectations are met and 
         improves service reliability.
       </>
     ),
   },
   {
     title: 'Robust Security',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/safe-and-stable.svg').default,
     description: (
       <>
         Security is a top priority. Our API employs robust security measures to protect 
         your data and ensure the integrity and confidentiality of your transactions. 
-        Safeguard your critical business data with state-of-the-art encryption and compliance standards.
       </>
     ),
   },
